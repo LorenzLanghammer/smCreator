23418bb3c87777e9f16d236689e6379ca0ab550d
Lorenz_laha <uqhpf@student.kit.edu>
2025-10-24 23:54:48 +0200

    fix dbInterface

--- database/dbInterface.py
+++ database/dbInterface.py
@@ -1,64 +1,10 @@
 import sqlite3
 import os
+from structures import *
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
-db_path = os.path.join(script_dir, "db.db")  # adjust if needed
+db_path = os.path.join(script_dir, "db.db")
 
-
-
-def get_email_ig_without_ap():
-    conn = None
-    try:
-        conn = sqlite3.connect(db_path)
-    except sqlite3.Error as e:
-        print(f"Error connecting to database: {e}")
-    
-    cursor = conn.cursor()
-    cursor.execute("SELECT * from GmailProfiles where ig_used = 0 AND app_password is NULL Limit 1")
-
-    conn.commit()
-    
-    row = cursor.fetchone()
-    conn.close()
-    if row:
-        return (row[0], row[1],  row[2], row[3], row[4])
-
-def get_gmail_ig():
-    conn = None
-    try:
-        conn = sqlite3.connect(db_path)
-    except sqlite3.Error as e:
-        print(f"Error connecting to database: {e}")
-    
-    cursor = conn.cursor()
-    cursor.execute("SELECT * from GmailProfiles where ig_used = 0 Limit 1")
-    row = cursor.fetchone()
-    
-    #cursor.execute("UPDATE GmailProfiles SET ig_used = ? WHERE id = ?", (1, row[0]))
-    conn.commit()
-    
-    conn.close()
-    if row:
-        return (row[0], row[1],  row[2], row[3], row[4])
-    
-def get_gmx_ig():
-    conn = None
-    try:
-        conn = sqlite3.connect(db_path)
-    except sqlite3.Error as e:
-        print(f"Error connecting to database: {e}")
-    
-    cursor = conn.cursor()
-    cursor.execute("SELECT * from GmxProfiles where ig_used = 0 Limit 1")
-    row = cursor.fetchone()
-    
-    #cursor.execute("UPDATE GmxProfiles SET ig_used = ? WHERE id = ?", (1, row[0]))
-    conn.commit()
-    
-    conn.close()
-    if row:
-        print(row[0])
-        return (row[0], row[1],  row[2], row[3])
     
 
 def get_gmx_tt():
@@ -79,21 +25,7 @@ def get_gmx_tt():
     if row:
         print(row[0])
         return (row[0], row[1],  row[2], row[3])
-    
 
-
-def set_ig_used_gmx(profile_id):
-    conn = None
-    try:
-        conn = sqlite3.connect(db_path)
-    except sqlite3.Error as e:
-        print(f"Error connecting to database: {e}")
-
-    cursor = conn.cursor()
-    cursor.execute("UPDATE GmxProfiles SET ig_used = ? WHERE id = ?", (1, profile_id))
-    conn.commit()
-    conn.close()
-    
 
 def set_tt_used_gmx(profile_id):
     conn = None
@@ -106,8 +38,6 @@ def set_tt_used_gmx(profile_id):
     cursor.execute("UPDATE GmxProfiles SET tt_used = ? WHERE id = ?", (1, profile_id))
     conn.commit()
     conn.close()
-
-
 
 
 def copy_to_database_gmail(filename):
@@ -165,44 +95,7 @@ def copy_to_database_gmx(filename):
     conn.close()
 
 
-
-
-def add_app_password(id, app_password):
-    """Create a database connection to the SQLite database specified by db_file."""
-    print("adding app password")
-    conn = None
-    try:
-        conn = sqlite3.connect(db_path)
-    except sqlite3.Error as e:
-        print(f"Error connecting to database: {e}")
-
-    cursor = conn.cursor()
-    cursor.execute("UPDATE GmailProfiles SET app_password = ? WHERE id = ?", (app_password, id))
-    conn.commit()
-    print("App password added successfully.")
-
-    cursor.close()
-    conn.close()
-
-
-def add_instagram_account(username, password, email, two_fa):
-    """Create a database connection to the SQLite database specified by db_file."""
-    print("adding instagram account")
-    conn = None
-    try:
-        conn = sqlite3.connect(db_path)
-    except sqlite3.Error as e:
-        print(f"Error connecting to database: {e}")
-    cursor = conn.cursor()
-    cursor.execute("INSERT INTO InstagramAccounts (username, password, email, two_fa) VALUES (?, ?, ?, ?)", 
-                   (username, password, email, two_fa))
-    conn.commit()
-    print("Instagram account added successfully.")
-    cursor.close()
-    conn.close()
-
-
-def add_tiktok_account(username, password, email, sessionid_ss, sessionid, sid_tt, sid_guard, uid_tt, uid_tt_ss, sid_ucp_v1):
+def add_tiktok_account(username, password, email, cookies, country):
     print("adding tiktok account")
     conn = None
     try:
@@ -211,8 +104,8 @@ def add_tiktok_account(username, password, email, sessionid_ss, sessionid, sid_t
         print(f"Error connecting to database: {e}")
 
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO TiktokProfiles (username, password, email, sessionid_ss, sessionid, sid_tt, sid_guard, uid_tt, uid_tt_ss, sid_ucp_v1) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", 
-                   (username, password, email, sessionid_ss, sessionid, sid_tt, sid_guard, uid_tt, uid_tt_ss, sid_ucp_v1))
+    cursor.execute("INSERT INTO TiktokProfiles (username, password, email, cookies, country) VALUES (?, ?, ?, ?, ?)", 
+                   (username, password, email, cookies, country))
     conn.commit()
     print("Tiktok account added successfully.")
 
@@ -247,6 +140,22 @@ def get_gmx_by_id(id):
     row = cursor.fetchone()
     conn.close()
     return row
+
+
+def get_random_tt():
+    conn = None
+    try:
+        conn = sqlite3.connect(db_path)
+    except sqlite3.Error as e:
+        print(f"Error connecting to database")
+    
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT * from TiktokProfiles WHERE Country IS NOT NULL ORDER BY RANDOM() LIMIT 1")
+    row = cursor.fetchone()
+    conn.close()
+    return Account(row[1], row[2], row[3], row[4], row[5])
+
+
 
 #get_all_gmx()
 #get_gmx_ig()
